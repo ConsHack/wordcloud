391b5ef3a393cb64bc693d600eef3881b4033198
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-06-20 16:30:07 +0100

    Comparison cloud

--- 2_wordcloud.R
+++ 2_wordcloud.R
@@ -16,7 +16,7 @@ library(tm)
 
 # WORDCLOUD
 # create corpus
-corp <- VCorpus (DirSource (1_parsed))
+corp <- VCorpus (DirSource ('1_parsed'))
 # sort chracters
 corp <- tm_map(corp, removePunctuation)
 corp <- tm_map(corp, content_transformer(tolower))
@@ -25,15 +25,17 @@ corp <- tm_map(corp, function(x)removeWords(x,stopwords()))
 # create terms matrix
 term.matrix <- TermDocumentMatrix(corp)
 term.matrix <- as.matrix(term.matrix)
-colnames(term.matrix) <- c("SOTU 2010","SOTU 2011")
-# generate wordcloud
-commonality.cloud(term.matrix,max.words=300,random.order=FALSE,
+comparison.cloud(term.matrix,max.words=100,random.order=FALSE,
+                 colors=brewer.pal(8, 'Dark2'))
+commonality.cloud(term.matrix,max.words=100,random.order=FALSE,
                   colors=brewer.pal(8, 'Dark2'))
 
-# get top 100
-sorted.instances <- sort(term.matrix[,1], TRUE)
-top.hundred <- sorted.instances[1:100]
-top.hundred <- data.frame (word=names(top.hundred),
-                           count=top.hundred)
-write.table (top.hundred, file='top_hundred.txt',
+# get top 100 -- TODO softcode
+sorted.lpi <- sort(term.matrix[,1], TRUE)
+sorted.predicts <- sort(term.matrix[,3], TRUE)
+comp.table <- data.frame (lpi.count=sorted.lpi,
+                          lpi.words=names (sorted.lpi),
+                          predicts.count=sorted.predicts,
+                          predicts.words=names (sorted.predicts))
+write.table (comp.table[1:100,], file='top_hundred.txt',
              quote=FALSE, row.names=FALSE, col.names=FALSE)
